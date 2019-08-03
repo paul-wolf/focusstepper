adc2696dc4f0dd07900904afa98c304456436b77
Ubuntu <ubuntu@ip-172-31-31-180.eu-central-1.compute.internal>
2019-08-03 07:23:30 +0000

    Add put command

--- put.py
+++ put.py
@@ -11,24 +11,21 @@ from dotenv import load_dotenv
 
 from s3 import get_s3, store_stream_s3, get_matching_s3_keys, get_object_to_file
 
-
 load_dotenv()
 BUCKET = os.environ.get("BUCKET")
 PATH_DATA = os.environ.get("PATH_DATA", "./data")
 
-def convert_stack(stack):
+def put_files(stack, spec):
     p = os.path.join(PATH_DATA, stack)
     if not os.path.exists(p):
         print("Path does not exist: {}".format(p))
         raise SystemExit
-    path = os.path.join(PATH_DATA, stack, "*.nef")
-    print("Stack data: ", path)
+    path = os.path.join(PATH_DATA, stack, spec)
     files = glob.glob(path)
     for fp in files:
-        dest = fp.replace(".nef", ".tiff")
-        print("converting: {} => {}".format(fp, dest))
-        o = subprocess.call(['convert', fp, dest])
-        print(o)
+        key = os.path.join(stack, fp.split("/")[-1])
+        print("uploading: {} => {}".format(fp, key))
+        store_stream_s3(BUCKET, open(fp, 'rb'), key)
 
 if __name__ == "__main__":
-    sys.exit(convert_stack(sys.argv[1]))
+    sys.exit(put_files(sys.argv[1], sys.argv[2]))
